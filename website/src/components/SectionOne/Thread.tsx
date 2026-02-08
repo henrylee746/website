2fab8594bb6a982927473efb469453e01371a529
henrylee746 <henry.lee746@gmail.com>
2026-02-08 15:05:13 -0500

    Disabled thread animation on mobile

--- website/src/components/SectionOne/Thread.tsx
+++ website/src/components/SectionOne/Thread.tsx
@@ -1,14 +1,20 @@
 "use client";
 import Threads from "../ui/threads";
+import { useIsMobile } from "../mobile";
 
 export const ThreadsBackground = () => {
+    const isMobile = useIsMobile();
     return (
-        <div className="absolute inset-0">
-            <Threads
-                amplitude={5}
-                distance={0.6}
-                enableMouseInteraction={false}
-            />
-        </div>
+        <>
+            {isMobile ? null : (
+                <div className="absolute inset-0">
+                    <Threads
+                        amplitude={5}
+                        distance={0.6}
+                        enableMouseInteraction={false}
+                    />
+                </div>
+            )}
+        </>
     );
 };
